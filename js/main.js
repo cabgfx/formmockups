a81af24db2de81349af9c93d7f4816b4819f568c
Casper Klenz-Kitenge <hi@cabgfx.com>
2013-10-31 14:39:18 +0100

    Basic tools setup for quick tweaks to form display

--- js/main.js
+++ js/main.js
@@ -1,1 +1,37 @@
+$(function() {
+  
+  $('#bold-labels-toggle').change(function() {
+    $('form label').toggleClass('bold');
+  });
 
+  $('#block-labels-toggle').change(function() {
+    $('form label').toggleClass('block');
+  });
+
+  $('#form-styles-toggle').change(function() {
+    $('#tools style').toggleClass('hidden');
+  });
+
+  $('#font-family').keyup(function() {
+    var $value = $(this).val(),
+        $form = $('form');
+    
+    if ($value == '') {
+      $form.removeAttr('style');
+    } else {
+      $form.attr('style', function() { return "font-family: "+ $value +";" });
+    }
+
+  });
+  
+  $('#page-background').keyup(function() {
+    var $value = $(this).val(),
+        $page = $('body');
+
+    if ($value == '') {
+      $page.removeAttr('style');
+    } else {
+      $page.attr('style', function() { return "background: "+ $value +";" });
+    }
+  });
+});
